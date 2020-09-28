d142d6393fa824bcb4861f6843e25e9aeeb31458
JT <48796307+JT-Banks@users.noreply.github.com>
2020-09-28 18:10:44 -0500

    Update serviceWorker.js

--- movieDB-app/serviceWorker.js
+++ movieDB-app/serviceWorker.js
@@ -25,8 +25,8 @@ export function register(config) {
     // The URL constructor is available in all browsers that support SW.
     const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
     if (publicUrl.origin !== window.location.origin) {
-      // Our service worker won't work if PUBLIC_URL is on a different origin
-      // from what our page is served on. This might happen if a CDN is used to
+      // Service worker won't work if PUBLIC_URL is on a different origin
+      // from what page is served on. This might happen if a CDN is used to
       // serve assets; see https://github.com/facebook/create-react-app/issues/2374
       return;
     }
@@ -35,7 +35,7 @@ export function register(config) {
       const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
 
       if (isLocalhost) {
-        // This is running on localhost. Let's check if a service worker still exists or not.
+        // This is running on localhost. Check if a service worker still exists or not.
         checkValidServiceWorker(swUrl, config);
 
         // Add some additional logging to localhost, pointing developers to the
